2d0ba22713cacf8fe6dc521c5e2afdee04d3b3e6
Mohammad Hassan <95434322+CriDeZ@users.noreply.github.com>
2023-01-19 11:07:01 +0200

    Exersices of js

---
+++ exersice1/yes_no.js
@@ -0,0 +1,10 @@
+function makesure(check){
+    if(check ===true || check ===1)
+        return console.log("Yes");
+    else
+        return console.log("False");    
+}
+makesure(1)
+makesure(true)
+makesure(0)
+makesure(false)
