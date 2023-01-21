0062040a6e2193aeed84c20c5ddb9b0241ec46be
Mohammad Hassan <95434322+CriDeZ@users.noreply.github.com>
2023-01-21 14:54:16 +0200

    Implementing functions

---
+++ Implement_Functionality/implement_ex7.js
@@ -0,0 +1,15 @@
+// filter method
+let numbers = [1, 2, 3, 4, 5];
+let evenNumbers = numbers.filter( (num) => num % 2 === 0);
+console.log(evenNumbers); 
+
+// ForEach method
+
+let numbers1 = [1, 2, 3, 4, 5];
+numbers1.forEach( (num) => console.log(num * 2)); 
+
+
+// mapMethod
+let numbers2 = [1, 2, 3, 4, 5];
+let doubleNumbers = numbers2.map( (num) => num * 2);
+console.log(doubleNumbers); 
---
+++ Implement_Functionality/peirmeter_rectangle_ex8.js
@@ -0,0 +1,5 @@
+function perimeter(len,wid){
+    return 2*(len+wid)
+}
+console.log(perimeter(20,10));
+console.log(perimeter(2,9));
